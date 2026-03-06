c367b68b6942dc77882c9f1551011e78a8185bb5
OjeeAfolabi <afolabiojee@gmail.com>
2026-03-06 16:12:20 +0100

    routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import ImageSlider from "./components/ImageSlider/ImageSlider";
 import NavBar from "./components/NavBar/NavBar";
-
+import {createBrowserRouter,Route } from "react-router-dom";
 function App() {
   return (
     <div className="layout">
