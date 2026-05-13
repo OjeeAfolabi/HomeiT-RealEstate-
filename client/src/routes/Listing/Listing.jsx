332ce10df7d1f5d6f503144e48178f654353cda4
Ojee Afolabi <afolabiojee@gmail.com>
2026-05-14 00:37:58 +0100

    features

--- client/src/routes/Listing/Listing.jsx
+++ client/src/routes/Listing/Listing.jsx
@@ -1,6 +1,8 @@
 import './listing.scss';
 const Listing = () => {
-  return <div className='listing'>Featured</div>;
+  return <div className='listing'>Featured
+    home listings will be displayed here.
+  </div>;
 };
 
 export default Listing;
